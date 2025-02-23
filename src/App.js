42f4f2e98e15cb2034aa0899ddb39d5a55705260
Estelle-Prn <peroni.estelle@gmail.com>
2025-02-23 16:33:15 +0100

    added the "rel="noreferrer"" in the app.js file

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ function App() {
       <Weather defaultCity="Rome" />
       <footer>
         Coded by{" "}
-        <a href="https://github.com/estelle-prn" target="_blank">
+        <a
+          href="https://github.com/estelle-prn"
+          target="_blank"
+          rel="noreferrer"
+        >
           Estelle Peroni
         </a>
         , open-sourced on{" "}
         <a
           href="https://github.com/Estelle-Prn/Weather-Project"
           target="_blank"
+          rel="noreferrer"
         >
           GitHub
         </a>
         , and hosted on{" "}
-        <a href="https://www.netlify.com" target="_blank">
+        <a href="https://www.netlify.com" target="_blank" rel="noreferrer">
           Netlify{" "}
         </a>
       </footer>
